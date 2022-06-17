4e62002563546bd7963ca602d804ebef4de37247
irakibul7 <irakibul568@gmail.com>
2022-06-17 17:27:20 +0600

    Add code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reduce the file size of base64 image
+
+Use the HTML to load the image in the input and see the magic happen 😅  
---
+++ main.js
@@ -0,0 +1,133 @@
+/**
+ * Resize a base 64 Image
+ * @param {String} base64Str - The base64 string (must include MIME type)
+ * @param {Number} MAX_WIDTH - The width of the image in pixels
+ * @param {Number} MAX_HEIGHT - The height of the image in pixels
+ */
+async function reduce_image_file_size(base64Str, MAX_WIDTH = 450, MAX_HEIGHT = 450) {
+    let resized_base64 = await new Promise((resolve) => {
+        let img = new Image()
+        img.src = base64Str
+        img.onload = () => {
+            let canvas = document.createElement('canvas')
+            let width = img.width
+            let height = img.height
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width
+                    width = MAX_WIDTH
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height
+                    height = MAX_HEIGHT
+                }
+            }
+            canvas.width = width
+            canvas.height = height
+            let ctx = canvas.getContext('2d')
+            ctx.drawImage(img, 0, 0, width, height)
+            resolve(canvas.toDataURL()) // this will return base64 image results after resize
+        }
+    });
+    return resized_base64;
+}
+
+
+async function image_to_base64(file) {
+    let result_base64 = await new Promise((resolve) => {
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => resolve(fileReader.result);
+        fileReader.onerror = (error) => {
+            console.log(error)
+            alert('An Error occurred please try again, File might be corrupt');
+        };
+        fileReader.readAsDataURL(file);
+    });
+    return result_base64;
+}
+
+async function process_image(file, min_image_size = 300) {
+    const res = await image_to_base64(file);
+    if (res) {
+        const old_size = calc_image_size(res);
+        if (old_size > min_image_size) {
+            const resized = await reduce_image_file_size(res);
+            const new_size = calc_image_size(resized);
+            console.log('new_size=> ', new_size, 'KB');
+            console.log('old_size=> ', old_size, 'KB');
+            return resized;
+        } else {
+            console.log('image already small enough')
+            return res;
+        }
+
+    } else {
+        console.log('return err')
+        return null;
+    }
+}
+
+/*- NOTE: USE THIS JUST TO GET PROCESSED RESULTS -*/
+async function preview_image() {
+    const file = document.getElementById('file');
+    const image = await process_image(file.files[0]);
+    // console.log(image)
+}
+
+/*- NOTE: USE THIS TO PREVIEW IMAGE IN HTML -*/
+async function preview_image() {
+    const file = document.getElementById('file');
+    const res = await image_to_base64(file.files[0])
+   
+    if (res) {
+        
+        const olds = calc_image_size(res)
+        const resized = await reduce_image_file_size(res);
+        const news = calc_image_size(resized);
+
+        document.getElementById("svg_preview").style.display = "none";
+        document.getElementById("img_preview").style.display = "block";
+        
+        document.getElementById("svg_preview2").style.display = "none";
+        document.getElementById("img_preview2").style.display = "block";
+
+        document.getElementById("old").src = res;
+        document.getElementById("new").src = resized;
+        
+
+        document.getElementById("old_img_size").innerHTML = olds + 'KB';
+        document.getElementById("new_img_size").innerHTML = news + 'KB';
+
+        var old_img = new Image(); 
+        old_img.src = res;
+        old_img.onload = function(){
+            document.getElementById("old_img_height").innerHTML = old_img.height + 'px';
+            document.getElementById("old_img_width").innerHTML = old_img.width + 'px';
+        };
+
+        var new_img = new Image(); 
+        new_img.src = resized;
+        new_img.onload = function(){
+            document.getElementById("new_img_height").innerHTML = new_img.height + 'px';
+            document.getElementById("new_img_width").innerHTML = new_img.width + 'px';
+        };
+
+        
+        
+        
+    } else {
+        console.log('return err')
+    }
+}
+
+
+function calc_image_size(image) {
+    let y = 1;
+    if (image.endsWith('==')) {
+        y = 2
+    }
+    const x_size = (image.length * (3 / 4)) - y
+    return Math.round(x_size / 1024)
+}
